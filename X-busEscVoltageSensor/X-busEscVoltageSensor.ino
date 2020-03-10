71a11f89349a1ac0ea4ff791b3f47fd0d517889a
Anders Hoglund <AndWho@gmail.com>
2020-03-10 12:39:52 +0100

    Use defs from spektrum header file. With a small modification.

--- X-busEscVoltageSensor/X-busEscVoltageSensor.ino
+++ X-busEscVoltageSensor/X-busEscVoltageSensor.ino
@@ -8,6 +8,19 @@
 
 #include <Wire.h>
 
+// A few basic types Spektrum depends on
+#define INT8 char
+#define INT16 short int
+#define INT32 long int
+#define INT64 long long int
+
+#define UINT8 unsigned char
+#define UINT16 unsigned short int
+#define UINT32 unsigned long int
+#define UINT64 unsigned long long int
+
+#include "spektrumTelemetrySensors.h"
+
 // select the analog voltage input pin
 #define SENSOR_PIN A0    
 
@@ -17,11 +30,6 @@
 
 #define ADC_SCALE 5/1023
 #define SCALE ((double)((RESISTOR_LOW + RESISTOR_HIGH) / RESISTOR_LOW) * ADC_SCALE)
-
-#define INT8 char
-#define INT16 short int
-#define UINT8 unsigned char
-#define UINT16 unsigned short int
 
 #define I2C_SDA_PIN A4
 #define I2C_SCL_PIN A5
@@ -44,66 +52,6 @@ typedef union
   unsigned int value;
 } endianBuff_u;
 
-typedef struct
-{
-  UINT8 identifier;       // Source device = 0x20
-  UINT8 sID;              // Secondary ID
-  UINT16 RPM;             // RPM, 10RPM (0-655340 RPM).    0xFFFF --> "No data"
-  UINT16 voltsInput;      // Volts, 0.01v (0-655.34V).     0xFFFF --> "No data"
-  UINT16 tempFET;         // Temperature, 0.1C (0-999.8C)  0xFFFF --> "No data"
-  UINT16 currentMotor;    // Current, 10mA (0-655.34A).    0xFFFF --> "No data"
-  UINT16 tempBEC;         // Temperature, 0.1C (0-999.8C)  0x7FFF --> "No data"
-  UINT8 currentBEC;       // BEC Current, 100mA (0-25.4A). 0xFF ----> "No data"
-  UINT8 voltsBEC;         // BEC Volts, 0.05V (0-12.70V).  0xFF ----> "No data"
-  UINT8 throttle;         // 0.5% (0-127%).                0xFF ----> "No data"
-  UINT8 powerOut;         // Power Output, 0.5% (0-127%).  0xFF ----> "No data"
-} STRU_TELE_ESC;
-
-typedef struct
-{
-  UINT8 identifier;        // Source device = 0x7E
-  UINT8 sID;               // Secondary ID
-  UINT16 microseconds;     // microseconds between pulse leading edges
-  UINT16 volts;            // 0.01V increments
-  INT16 temperature;       // degrees F
-  INT8 dBm_A;              // Average signal for A antenna in dBm
-  INT8 dBm_B;              // Average signal for B antenna in dBm.
-                           // If only 1 antenna, set B = A
-} STRU_TELE_RPM;
-
-typedef union
-{
-  UINT8 raw[16];
-  //STRU_TELE_RTC rtc;
-  //STRU_TELE_QOS qos;
-  STRU_TELE_RPM rpm;
-  //STRU_TELE_FRAMEDATA frame;
-  //STRU_TELE_ALT alt;
-  //STRU_TELE_SPEED speed;
-  //STRU_TELE_ENERGY_DUAL eDual;
-  //STRU_TELE_VARIO_S varioS;
-  //STRU_TELE_G_METER accel;
-  //STRU_TELE_JETCAT jetcat;
-  //STRU_TELE_JETCAT2 jetcat2;
-  //STRU_TELE_GPS_LOC gpsloc;
-  //STRU_TELE_GPS_STAT gpsstat;
-  //STRU_TELE_GYRO gyro;
-  //STRU_TELE_ATTMAG attMag;
-  //STRU_TELE_POWERBOX powerBox;
-  STRU_TELE_ESC escGeneric;
-  //STRU_TELE_LAPTIMER lapTimer;
-  //STRU_TELE_TEXTGEN textgen;
-  //STRU_TELE_FUEL fuel;
-  //STRU_TELE_MAH mAh;
-  //STRU_TELE_DIGITAL_AIR digAir;
-  //STRU_TELE_STRAIN strain;
-  //STRU_TELE_LIPOMON lipomon;
-  //STRU_TELE_LIPOMON_14 lipomon14;
-  //STRU_TELE_USER_16SU user_16SU;
-  //STRU_TELE_USER_16SU32U user_16SU32U;
-  //STRU_TELE_USER_16SU32S user_16SU32S;
-  //STRU_TELE_USER_16U32SU user_16U32SU;
-} UN_TELEMETRY;
 
 #ifdef USE_ESC_FRAME
 #define IDENTIFIER  TELE_DEVICE_ESC
@@ -148,7 +96,7 @@ void setup()
   pinMode(13,OUTPUT);
   digitalWrite(13,LOW);           // Debug LED and scope trigger
   
-  Wire.begin(TELE_DEVICE_ESC);    // join i2c bus with a slave address
+  Wire.begin(IDENTIFIER);         // join i2c bus with a slave address
   Wire.onRequest(requestEvent);   // register event
 }
 
@@ -169,8 +117,8 @@ void loop()
   unsigned short int centiVoltage= voltage *100;
 
 #ifdef USE_ESC_FRAME
-  TmBuffer.escGeneric.voltsInput = SwapEndian(centiVoltage);
-  TmBuffer.escGeneric.tempBEC    = UINT_NO_DATA_LE; // Odd...
+  TmBuffer.esc.voltsInput = SwapEndian(centiVoltage);
+  TmBuffer.esc.tempBEC    = UINT_NO_DATA_LE; // Odd...
 #endif
 
 #ifdef USE_RPM_FRAME
