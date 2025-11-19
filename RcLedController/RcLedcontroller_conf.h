f8902fbcd2770dc1c85a7d5c103f727911517cd1
Anders Hoglund <AndWho@gmail.com>
2025-11-19 18:48:25 +0100

    RcLedController, adapt to new Spektrum AUX ch numbering.

--- RcLedController/RcLedcontroller_conf.h
+++ RcLedController/RcLedcontroller_conf.h
@@ -13,20 +13,20 @@
 #endif
 
 // Spektrum channel order
-#define THRO 0
-#define AILE 1
+#define THRO 0   // Ch 1
+#define AILE 1   // Ch 2
 #define ELEV 2
 #define ROLL 3
 #define GEAR 4
-#define AUX1 5
-#define AUX2 6
-#define AUX3 7
-#define AUX4 8
-#define AUX5 9
+#define AUX6 5
+#define AUX7 6
+#define AUX8 7
+#define AUX9 8
+#define AUX10 9  //Ch 10
 
-// Only available at 22ms frame rate, not at 11ms.
-#define AUX6 10
-#define AUX7 11
+// Channels 11 and 12 Only available at 22ms frame rate, not at 11ms.
+#define AUX11 10  // Ch 11
+#define AUX12 11  // Ch 12
 
-#define LED_CONTROL_CHANNEL AUX7
+#define LED_CONTROL_CHANNEL AUX10
 
